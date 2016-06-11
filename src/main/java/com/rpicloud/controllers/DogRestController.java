9c1f7429a89ac3e5979104a66ad135441dc334a4
Kasper Nissen <kni@it-minds.dk>
2016-06-11 08:41:22 +0200

    dogs added

--- src/main/java/com/rpicloud/controllers/DogRestController.java
+++ src/main/java/com/rpicloud/controllers/DogRestController.java
@@ -21,6 +21,10 @@ public class DogRestController {
         col.add(new Dog("Mr. Vuff", "http://i.giphy.com/6lsW1YMkojWrC.gif", "2", "VuuuF!"));
         col.add(new Dog("Mr. Vuff1", "http://i.giphy.com/fpXxIjftmkk9y.gif", "2", "VuuuuuuuFFiii!"));
         col.add(new Dog("Mr. Vuff2", "http://i.giphy.com/l3nWl5bhBoim7glNu.gif", "2", "Aarh!"));
+        col.add(new Dog("Mr. Vuff3", "http://i.giphy.com/8belcgd05xKUM.gif", "2", "Vuf!"));
+        col.add(new Dog("Mr. Vuff4", "http://i.giphy.com/b9pK1N3lrYkCc.gif", "2", "Vuf Vuf!"));
+        col.add(new Dog("Mr. Vuff5", "http://i.giphy.com/XFl25q22vH2O4.gif", "2", "Vuf!"));
+        col.add(new Dog("Mr. Vuff6", "http://i.giphy.com/fW9Wg5uTY5HUc.gif", "2", "Vuf!"));
         return new ResponseEntity<>(col, HttpStatus.OK);
     }
 }
